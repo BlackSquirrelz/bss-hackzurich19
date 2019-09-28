3cbb705fe6ae82acb2a59bd4f8125e935a944e78
BlackSquirrelz <tobias.weisskopf.ch@gmail.com>
2019-09-28 09:43:11 +0200

    python_main

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import os
+def main():
+    print("python main function")
+    get_credentials()
 
-from twilio.rest import Client
+def get_credentials():
 
-# Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
+    import os
+    from twilio.rest import Client
+
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    welcome_message = "Welcome to the SBB - Text Adventure, are you ready to begin your adventure?"
+
+    message = client.messages.create(to=my_cell, from=my_twilio, body=welcome_message)
+    
+
+
+if __name__ == '__main__':
+    main()
