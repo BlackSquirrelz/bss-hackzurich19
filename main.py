ea27fbcf028ec048f78f9db5c7b2cfdc98a5ebeb
BlackSquirrelz <tobias.weisskopf.ch@gmail.com>
2019-09-29 08:47:41 +0200

    general updates

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import os
+from twilio.rest import Client
+import env_var
+
 def main():
     print("python main function")
-    get_credentials()
 
-def get_credentials():
+    account_sid = env_var.twilio_account_sid
+    auth_token = env_var.twilio_auth_token
+    my_twilio= env_var.twilio_phone
+    my_cell = '++41797725070'
 
-    import os
-    from twilio.rest import Client
-
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
 
-    welcome_message = "Welcome to the SBB - Text Adventure, are you ready to begin your adventure?"
+    # Build task actions that say something and listens for a repsonse.
+    welcome_actions = {
+        'actions': [
+            {'say': 'Welcome to SBB - Surprise Travel. Would you like to participate in the adventure and learn more about your destination?'},
+            {'listen': True}
+        ]
+    }
 
-    message = client.messages.create(to=my_cell, from=my_twilio, body=welcome_message)
-    
+    task = client.autopilot.assistants('UA482a5a7b43880b57c176b292967ea404') \
+        .tasks('UD55aaf72992fb0714d353fd0e225ea907')
+        .fetch()
+
+    print("Welcome Task has been created")
+    print(task.sid)
 
 
 if __name__ == '__main__':
